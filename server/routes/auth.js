014ab24f3e1058a014da2eb768e1dbd17dbb2554
arifwp <arifwahyu696@gmail.com>
2023-10-18 00:26:15 +0700

    add jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 
 const authRouter = express.Router();
+const jwt = require("jsonwebtoken");
 
 authRouter.post("/api/signup", async (req, res) => {
   try {
@@ -24,6 +25,29 @@ authRouter.post("/api/signup", async (req, res) => {
     });
     user = await user.save();
     res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// sign in route
+// exercise
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "User with this email does not exists" });
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Incorrect password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({ ...user._doc, token });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
